831979231cbe2af615f0eb80e13aac225ec4b9fe
teshigahara <crnhrv@gmail.com>
2021-02-22 19:16:08 +0000

    add compuiter class and tests

---
+++ tests/computer.test.js
@@ -0,0 +1,15 @@
+import { Computer } from '../src/computer';
+import { Gameboard } from '../src/gameboard';
+
+let gameboard;
+
+beforeEach(() => {
+  gameboard = new Gameboard();
+});
+
+it('can take a turn', () => {
+  const computer = new Computer({ gameboard });
+  const spy = jest.spyOn(gameboard, 'receiveAttack');
+  computer.takeTurn();
+  expect(spy).toHaveBeenCalled();
+});
