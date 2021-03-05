c6195b2a61b250b4f94536e306163b518ad56993
teshigahara <crnhrv@gmail.com>
2021-03-05 18:23:19 +0000

    add ships data

---
+++ src/constants/ships.js
@@ -0,0 +1,6 @@
+const carrier = { length: 5, count: 1, class: 'ship', id: 'car' };
+const battleship = { length: 4, count: 2, class: 'ship', id: 'bat' };
+const cruiser = { length: 3, count: 3, class: 'ship', id: 'cru' };
+const destroyer = { length: 2, count: 4, class: 'ship', id: 'des' };
+
+export const SHIPS = [carrier, battleship, cruiser, destroyer];
